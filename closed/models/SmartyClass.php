56e8781263827695bd698ae30b2862eb901b0b3e
sapphirehead <bykovevg@yandex.ru>
2016-09-08 22:54:39 +0300

    little changes

--- closed/models/SmartyClass.php
+++ closed/models/SmartyClass.php
@@ -18,20 +18,11 @@ class SmartyClass extends \Smarty
 {
         public function __construct()
         {
-            parent::__construct();// это тоже работает: $this->__construct() и это: Smarty::__construct();
+            parent::__construct();
             $this->template_dir = 'mytemplates/';
             $this->compile_dir  = 'mytemplates_c/';
             $this->config_dir   = 'myconfigs/';
             $this->cache_dir    = 'mycache/';
-
-            //$this->caching = true;// по умолчанию кэширование выключено
-            $this->debugging = false;// если true будет выводиться окно консоли дебаггера Смарти при выводе всех шаблонов
-            // $this->cache_lifetime = 120;
-            //$this->compile_check = true;
-            //$this->force_compile = true;
-            //$tpl = $this->fetch('Exempl.tpl');echo $tpl;
-            //print_r($this->get_temlpate_vars());
-            //if($this->templateExists('footer.tpl')) echo "Yes";
-            $this->error_reporting = E_ALL; // LEAVE E_ALL DURING DEVELOPMENT
+            //$this->error_reporting = E_ALL; // LEAVE E_ALL DURING DEVELOPMENT
         }
 }
